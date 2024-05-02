f5cf9ffeec0c6230407097101b8020c56c875316
Mario Bodenmüller <mario.bodenmueller@gmail.com>
2024-05-02 13:02:49 +0200

    initial commit

---
+++ ui/src/app/components/button-input/button-input.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonInputComponent } from './button-input.component';
+
+describe('ButtonInputComponent', () => {
+  let component: ButtonInputComponent;
+  let fixture: ComponentFixture<ButtonInputComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ButtonInputComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ButtonInputComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
